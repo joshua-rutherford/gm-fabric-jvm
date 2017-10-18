149503b735005099812b2471a9aa0d696011f807
Garth Hershfield <garth.hershfield@deciphertechstudios.com>
2017-10-18 16:56:12 -0400

    Issue-55 v002: integration-verify is able to discover the answers.

--- integration-setup/src/main/resources/example02.sh
+++ integration-setup/src/main/resources/example02.sh
@@ -14,7 +14,7 @@ source dependencies.sh
 #scala -classpath ${INTEGRATION_CLASSPATH}:gm-fabric-core-integration-0.1.5-SNAPSHOT.jar:. com.deciphernow.integration.IntegrationTests
 export ANNOUNCE_ADMIN_PORT=:45000
 export ANNOUNCE_HTTP_PORT=:45001
-scala -classpath ${INTEGRATION_CLASSPATH}:gm-fabric-core-integration-0.1.5-SNAPSHOT.jar:. -Dcom.deciphernow.announcement.config.os.env.adminPort=ANNOUNCE_ADMIN_PORT -Dcom.deciphernow.announcement.config.os.env.httpPort=ANNOUNCE_HTTP_PORT com.deciphernow.integration.IT001 /tmp/001.txt
+scala -classpath ${INTEGRATION_CLASSPATH}:gm-fabric-core-integration-setup-0.1.5-SNAPSHOT.jar:. -Dcom.deciphernow.announcement.config.os.env.adminPort=ANNOUNCE_ADMIN_PORT -Dcom.deciphernow.announcement.config.os.env.httpPort=ANNOUNCE_HTTP_PORT com.deciphernow.integration.IT001 /tmp/001.txt
 
 
 )
